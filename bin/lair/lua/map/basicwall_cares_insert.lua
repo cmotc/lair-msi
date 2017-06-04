30b39ae01603d6bcab9f5064412cea71ce9c5ffd
idk <problemsolver@openmailbox.org>
2017-06-04 05:02:49 -0400

    'cmotc' updated makefile to do new windows version, don't do assertions on Unix 170604050249

--- bin/lair/lua/map/basicwall_cares_insert.lua
+++ bin/lair/lua/map/basicwall_cares_insert.lua
@@ -32,13 +32,13 @@ function medwall_cares_insert(vari)
         if lesser_than(where_in_floor_get_x(), 2*32) then
                 decided_to="true"
         end
-        if greater_than(where_in_floor_get_x(), where_is_floor_farcorner_x() - (3*32) then
+        if greater_than(where_in_floor_get_x(), where_is_floor_farcorner_x() - (3*32)) then
                 decided_to="true"
         end
         if lesser_than(where_in_floor_get_y(), 2*32) then
                 decided_to="true"
         end
-        if greater_than(where_in_floor_get_y(), where_is_floor_farcorner_y() - (3*32) then
+        if greater_than(where_in_floor_get_y(), where_is_floor_farcorner_y() - (3*32)) then
                 decided_to="true"
         end
         print("Should a medium wall be inserted? ", decided_to)
@@ -61,7 +61,7 @@ function thinwall_cares_insert(vari)
         if lesser_than(where_in_floor_get_y(), 1*32) then
                 decided_to="true"
         end
-        if greater_than(where_in_floor_get_y(), where_is_floor_farcorner_y() - (2*32) then
+        if greater_than(where_in_floor_get_y(), where_is_floor_farcorner_y() - (2*32)) then
                 decided_to="true"
         end
         print("Should a thick wall be inserted? ", decided_to)
